ffd2475f5c7f80ad0c0164d10d3ec4265390beb0
BehnamEbrahimi <46883116+BehnamEbrahimi@users.noreply.github.com>
2019-01-26 21:22:09 +1100

    Create tuples.py

---
+++ Tuples/tuples.py
@@ -0,0 +1,26 @@
+# lists are mutable but tuples are immutable
+# Mutable
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
+
+print(list_1)
+print(list_2)
+
+list_1[0] = 'Art'  # because lists are mutable, by changing the value of list_1, list_2 will be changed accordingly
+
+print(list_1)
+print(list_2)
+
+
+# Immutable
+tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+tuple_2 = tuple_1
+
+print(tuple_1)
+print(tuple_2)
+
+# the line below can not run because, tuples can not be changed
+# tuple_1[0] = 'Art'
+
+print(tuple_1)
+print(tuple_2)
