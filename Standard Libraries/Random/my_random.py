0bb9de87ae2693379892f7df58b7d049695524fe
BehnamEbrahimi <46883116+BehnamEbrahimi@users.noreply.github.com>
2019-01-27 17:34:02 +1100

    Create my_random.py

---
+++ Standard Libraries/Random/my_random.py
@@ -0,0 +1,5 @@
+import random
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+random_course = random.choice(courses)  # this method takes a list and returns a random member
+print(random_course)
