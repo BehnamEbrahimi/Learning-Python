d061323d28efea73972512b8883439956d8f743b
BehnamEbrahimi <46883116+BehnamEbrahimi@users.noreply.github.com>
2019-01-28 20:23:52 +1100

    Update lists.py

--- Lists/lists.py
+++ Lists/lists.py
@@ -66,3 +66,12 @@ print(converted_str.split(', '))
 # Empty Lists
 empty_list = []
 empty_list = list()
+
+# list slicing
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#          0, 1, 2, 3, 4, 5, 6, 7, 8, 9 : positive index
+#        -10,-9,-8,-7,-6,-5,-4,-3,-2,-1 : negative index
+# list[start:end:step]
+print(my_list[-8:6])  # we can combine positive and negative index
+print(my_list[0:-1: 2]) # every other value
+print(my_list[::-1])  # it is like reverse. by leaving out the start and end it starts from the beginning to the end
