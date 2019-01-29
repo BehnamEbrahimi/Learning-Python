1adbccf7c2b0003dbd93ebf8973a7a8d96d876bc
BehnamEbrahimi <46883116+BehnamEbrahimi@users.noreply.github.com>
2019-01-29 13:21:51 +1100

    Update sets.py

--- Sets/sets.py
+++ Sets/sets.py
@@ -11,3 +11,18 @@ print(cs_courses.difference(art_courses))  # difference of two sets.
 # Empty Sets
 empty_set = {}  # This isn't right! It's a dict
 empty_set = set()
+
+# Adding a member to the set
+art_courses.add('Painting')
+print(art_courses)
+
+# Set Comprehensions
+nums = [1, 1, 2, 1, 3, 4, 3, 4, 5, 5, 6, 7, 8, 7, 9, 9]
+# converting a list to a set. conventional way:
+my_set = set()
+for n in nums:
+    my_set.add(n)
+print(my_set)
+# converting a list to a set using set comprehensions:
+my_set = {n for n in nums}
+print(my_set)
