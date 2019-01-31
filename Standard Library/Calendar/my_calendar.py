4e8c0fa4b00b917d5dac0aa71d1f987c05457d92
BehnamEbrahimi <46883116+BehnamEbrahimi@users.noreply.github.com>
2019-01-31 17:23:32 +1100

    Update my_calendar.py

--- Standard Library/Calendar/my_calendar.py
+++ Standard Library/Calendar/my_calendar.py
@@ -2,3 +2,4 @@ import calendar
 
 print(calendar.isleap(2019))
 print(calendar.isleap(2020))
+print(calendar.monthrange(2018, 1)) # returns a tuple. 0 is the weekday of the start of the month and 1 is the total number of days in that month
