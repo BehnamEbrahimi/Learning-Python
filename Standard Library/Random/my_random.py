017f7a99cfd1ad19ac276d8471cffbdda1c22ae8
BehnamEbrahimi <46883116+BehnamEbrahimi@users.noreply.github.com>
2019-01-31 13:51:10 +1100

    Update my_random.py

--- Standard Library/Random/my_random.py
+++ Standard Library/Random/my_random.py
@@ -1,5 +1,48 @@
-import random
+import random  # not suitable to generate cryptoghraphy, uses secrets module instead
+
+print(random.random())  # random method, between 0 (inclusive) and 1 (non-inclusive)
+print(random.uniform(1, 10))  # floating value beween two numbers
+
+print(random.randint(1, 6))  # random integer: both inclusive
+print(random.randint(0, 1))  # simulating a coin
 
 courses = ['History', 'Math', 'Physics', 'CompSci']
-random_course = random.choice(courses)  # this method takes a list and returns a random member
-print(random_course)
+print(random.choice(courses))  # choice method: this method takes a list and returns a random member
+colors = ['Red', 'Black', 'Green']
+print(random.choices(colors, k=10))  # choices method: this method takes a list and the number of rounds and returns a list. all values in the list are equal in terms of the chance to be in the returning list. of course, if k is greater than the number of original list members, there will be duplicates for sure.
+print(random.choices(colors, weights=[18, 18, 2], k=10))  # Red chance is 18/38, Black chance is 18/38 and Green is 2/38
+
+deck = list(range(1, 52))
+random.shuffle(deck)  # returns None
+print(deck)
+print(random.sample(deck, k=5))  # unlike choices method, this method only returns unique values. k cannot be greater 51.
+
+# practical example
+
+''' Super simple module to create basic random data for tutorials'''
+first_names = ['John', 'Jane', 'Corey', 'Travis', 'Dave', 'Kurt', 'Neil', 'Sam', 'Steve', 'Tom', 'James', 'Robert', 'Michael', 'Charles', 'Joe', 'Mary', 'Maggie', 'Nicole', 'Patricia', 'Linda', 'Barbara', 'Elizabeth', 'Laura', 'Jennifer', 'Maria']
+
+last_names = ['Smith', 'Doe', 'Jenkins', 'Robinson', 'Davis', 'Stuart', 'Jefferson', 'Jacobs', 'Wright', 'Patterson', 'Wilks', 'Arnold', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White', 'Harris', 'Martin']
+
+street_names = ['Main', 'High', 'Pearl', 'Maple', 'Park', 'Oak', 'Pine', 'Cedar', 'Elm', 'Washington', 'Lake', 'Hill']
+
+fake_cities = ['Metropolis', 'Eerie', "King's Landing", 'Sunnydale', 'Bedrock', 'South Park', 'Atlantis', 'Mordor', 'Olympus', 'Dawnstar', 'Balmora', 'Gotham', 'Springfield', 'Quahog', 'Smalltown', 'Epicburg', 'Pythonville', 'Faketown', 'Westworld', 'Thundera', 'Vice City', 'Blackwater', 'Oldtown', 'Valyria', 'Winterfell', 'Braavos‎', 'Lakeview']
+
+states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
+for num in range(100):
+    first = random.choice(first_names)
+    last = random.choice(last_names)
+
+    phone = f'{random.randint(100, 999)}-555-{random.randint(1000,9999)}'
+
+    street_num = random.randint(100, 999)
+    street = random.choice(street_names)
+    city = random.choice(fake_cities)
+    state = random.choice(states)
+    zip_code = random.randint(10000, 99999)
+    address = f'{street_num} {street} St., {city} {state} {zip_code}'
+
+    email = first.lower() + last.lower() + '@bogusemail.com'
+
+    print(f'{first} {last}\n{phone}\n{address}\n{email}\n')
